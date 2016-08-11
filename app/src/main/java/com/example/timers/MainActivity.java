7382937304b43a10eb9b71821984e2d623577192
ghalibse <ghalibse@gmail.com>
2016-08-11 16:08:23 -0400

    After progress bar

--- app/src/main/java/com/example/timers/MainActivity.java
+++ app/src/main/java/com/example/timers/MainActivity.java
@@ -6,11 +6,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivityTAG_";
     private ProgressBar mProgressBar;
+    private TextView mTextView;
+    int counter=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,17 +21,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mProgressBar = (ProgressBar) findViewById(R.id.a_main_progress);
+        mTextView = (TextView) findViewById(R.id.coun_txt);
+
     }
 
     public void doMagic(View view) {
 
-        Handler handler = new Handler();
+
+        final Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-//                Log.d(TAG, "run: ");
+                Log.d(TAG, "run: " + Thread.currentThread());
 
-                mProgressBar.setVisibility(View.GONE);
+                handler.postDelayed(this, 1000);
+                mTextView.setText(String.valueOf(counter++));
+
+                mProgressBar.setMax(200);
+                mProgressBar.setProgress(counter);
+                mProgressBar.setVisibility(View.VISIBLE);
+/*
+                if(counter % 2 == 0)
+                    mProgressBar.setVisibility(View.INVISIBLE);
+                 else mProgressBar.setVisibility(View.VISIBLE);
+*/
+
             }
         }, 1000);
     }
